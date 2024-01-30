a10256ca9ce7edfaf698ccbcacfde02ec5edfd54
meshun074 <eshunmichael2tech1@yahoo.com>
2024-01-30 18:37:36 -0500

    left with some minor screening with parameters

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Genetic_Algorithm ga1 = new Genetic_Algorithm(50,4, 1.0F,0.1F,30);
+        Genetic_Algorithm ga1 = new Genetic_Algorithm(50,4, 1.0F,0.3F,500);
         ga1.start_GA();
     }
 }
